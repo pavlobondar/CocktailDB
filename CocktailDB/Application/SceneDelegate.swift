eef102b05d6ab25ea4cd3d5ab1f25005ea76ce46
Pavel Bondar <ivizey@gmail.com>
2020-09-09 12:03:45 +0300

    added a project structure

---
+++ CocktailDB/Application/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  CocktailDB
+//
+//  Created by Pavel Bondar on 09.09.2020.
+//  Copyright © 2020 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
