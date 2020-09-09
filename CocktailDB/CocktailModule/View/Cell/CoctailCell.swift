1307f298e4864b496bf9455e9a8288161d099552
Pavel Bondar <ivizey@gmail.com>
2020-09-09 23:50:32 +0300

    created cell ui

---
+++ CocktailDB/CocktailModule/View/Cell/CoctailCell.swift
@@ -0,0 +1,24 @@
+//
+//  CoctailCell.swift
+//  CocktailDB
+//
+//  Created by Pavel Bondar on 09.09.2020.
+//  Copyright © 2020 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class CoctailCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
